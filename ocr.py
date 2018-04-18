e0d921a081a550347a9b8492994c63420e987ef4
Gowtham Behara <gothsiddu@gmail.com>
2018-04-18 20:15:33 +0530

    Add img/ to .gitignore

--- ocr.py
+++ ocr.py
@@ -1,13 +1,15 @@
 import argparse
 import cv2
 import os
+import pytesseract
+from PIL import Image
 
 # construct the argument parse and parse the arguments
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument("-i", "--image", required=True, help="path to input image")
 arg_parser.add_argument("-p", "--preprocess", default="threshold", type=str, help="type of preprocessing to be done")
 
-args = vars(arg_parser)
+args = vars(arg_parser.parse_args())
 
 image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -19,3 +21,11 @@ elif "blur" == args["preprocess"]:
 
 filename = "{}.png".format(os.getpgid())
 cv2.imwrite(filename=filename, img=gray)
+
+text = pytesseract.image_to_string(Image.open(filename))
+os.remove(filename)
+print(text)
+
+cv2.imshow("Image", image)
+cv2.imshow("Output", gray)
+cv2.waitKey(0)
